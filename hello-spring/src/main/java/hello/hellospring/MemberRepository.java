7719a58a9eedf029e04d938f927adfd7112a447f
zion <lce322@naver.com>
2021-12-11 01:17:27 +0900

    Member(Domain)

---
+++ hello-spring/src/main/java/hello/hellospring/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello.hellospring.domain.Member;
+
+public interface MemberRepository {
+	Member save(Member member);
+	Optional<Member> findById(Long id);
+	Optional<Member> findByName(String name);
+	List<Member> findAll();
+}
