8dcfa3713af9e002e51cd2b13b043fcf501c22ca
zion <lce322@naver.com>
2021-12-12 01:03:25 +0900

    member test code

--- hello-spring/src/main/java/hello/hellospring/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/MemoryMemberRepository.java
@@ -44,5 +44,8 @@ public class MemoryMemberRepository implements MemberRepository {
 		// TODO Auto-generated method stub
 		return new ArrayList<>(store.values());
 	}
+	public void clearStore() {
+		store.clear();
+	}
 
 }
