1699a6f984922332db499e82bbcf1332ec3d2e8d
zion <lce322@naver.com>
2021-12-19 01:34:57 +0900

    AOP

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
@@ -64,6 +65,12 @@ public class SpringConfig {
 		//return new JpaMemberRepository(em);
 		
 	};
+	*/
+	/*
+	@Bean
+	public TimeTraceAop timeTraceAop() {
+		return new TimeTraceAop();
+	}
 	*/
 	
 
