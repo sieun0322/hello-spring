03660fcf071fd5ab5f20efd02a9f7f000a7a197e
zion <lce322@naver.com>
2021-12-26 03:05:05 +0900

    AppConfing 역할 구현 분리

--- hello-spring/src/main/java/hello/core/AppConfig.java
+++ hello-spring/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -11,10 +12,19 @@ public class AppConfig {
 
     //생성자 주입
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        
+        return new MemberServiceImpl(memberRepository());
+    }
+    //역할 구현 구분
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
