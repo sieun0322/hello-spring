f12a2f9873193a18b28f0314459be677bccb8b2b
zion <lce322@naver.com>
2022-01-15 16:04:19 +0900

    Spring Bean @PostConstruct @PreDestroy

--- hello-spring/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ hello-spring/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * 1. implements InitializingBean, DisposableBean
  * 인터페이스 초기화빈, 외부라이브러리 변경 불가능
@@ -14,6 +17,12 @@ import org.springframework.beans.factory.InitializingBean;
  * 외부라이브러리 초기화 종료 메서드 적용 가능
  * -destroyMethod
  * (inferred) 'close','shutdown' 이름의 베서드 자동 호출
+ *
+ * 3.@PostConstruct @PreDestroy
+ * 스프링 권장
+ * 자바 표준으로 스프링이 아니어도 사용가능
+ * 컴포넌트 스캔과 잘 활용
+ * 단점: 외부 라이브러리는 2. 사용해야한다.
  */
 public class NetworkClient  {
     private String url;
@@ -38,12 +47,13 @@ public class NetworkClient  {
      * 의존관계 주입이 끝나면,
      * @throws Exception
      */
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("init()");
         connect();
         call("초기화 연결 메세지");
     }
-
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("close");
         disconnect();
