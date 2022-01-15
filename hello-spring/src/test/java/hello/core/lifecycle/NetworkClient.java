ed5eb61da1ef2685b1b4c32e7ec01cebb4a1fd22
zion <lce322@naver.com>
2022-01-15 15:52:14 +0900

    Spring Bean 인터페이스 InitializingBean, DisposableBean (사용X)

--- hello-spring/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ hello-spring/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,17 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * 인터페이스 초기화빈, 외부라이브러리 변경 불가능
+ * 현재 사용 X
+ */
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient(){
         System.out.println("url = " + url);
-        connect();
-        call("초기화 연결 메세지");
-
     }
     public void setUrl(String url){
         this.url=url;
@@ -20,5 +24,22 @@ public class NetworkClient {
     }
     public void disconnect(){
         System.out.println("close: = " + url);
+    }
+
+    /**
+     * 의존관계 주입이 끝나면,
+     * @throws Exception
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet()");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy");
+        disconnect();
     }
 }
