0cafc29a5cf0da4a6269800078fd53ac1e7b57cb
zion <lce322@naver.com>
2022-01-15 15:59:16 +0900

    Spring Bean @Bean(initMethod = "init", destroyMethod = "close")

--- hello-spring/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ hello-spring/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -4,10 +4,18 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
+ * 1. implements InitializingBean, DisposableBean
  * 인터페이스 초기화빈, 외부라이브러리 변경 불가능
+ * 메서드 이름 변경 불가능
  * 현재 사용 X
+ *
+ * 2.@Bean(initMethod = "init", destroyMethod = "close")
+ * 초기화 종료 함수 지정 가능
+ * 외부라이브러리 초기화 종료 메서드 적용 가능
+ * -destroyMethod
+ * (inferred) 'close','shutdown' 이름의 베서드 자동 호출
  */
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient  {
     private String url;
 
     public NetworkClient(){
@@ -30,16 +38,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
      * 의존관계 주입이 끝나면,
      * @throws Exception
      */
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("afterPropertiesSet()");
+    public void init() throws Exception {
+        System.out.println("init()");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("destroy");
+    public void close() throws Exception {
+        System.out.println("close");
         disconnect();
     }
 }
