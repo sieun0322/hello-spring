c636418d102bb0d38c17cdd296186a2e2dfedd3a
zion <lce322@naver.com>
2022-01-23 17:11:17 +0900

    로그 추적기 V2 traceId, level 동기화 [스프링 핵심원리 - 고급편]

--- hello-spring/src/main/java/hello/advanced/app/v0/OrderControllerV0.java
+++ hello-spring/src/main/java/hello/advanced/app/v0/OrderControllerV0.java
@@ -1,7 +1,6 @@
-package hello.advanced.v0;
+package hello.advanced.app.v0;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- hello-spring/src/main/java/hello/advanced/app/v0/OrderServiceV0.java
+++ hello-spring/src/main/java/hello/advanced/app/v0/OrderServiceV0.java
@@ -1,7 +1,6 @@
-package hello.advanced.v0;
+package hello.advanced.app.v0;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Service
