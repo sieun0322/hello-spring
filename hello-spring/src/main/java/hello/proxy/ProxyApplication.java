6386f53ef0f9cc1ca07216152da69809d9326cf6
zion <lce322@naver.com>
2022-02-03 01:21:21 +0900

    v1. 인터페이스, 구체 클래스 -> 스프링 빈 등록 [스프링 핵심원리 - 고급편]

---
+++ hello-spring/src/main/java/hello/proxy/ProxyApplication.java
@@ -0,0 +1,19 @@
+package hello.proxy;
+
+import hello.core.AppConfig;
+import hello.proxy.config.AppV1Config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class ProxyApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ProxyApplication.class,args);
+    }
+}
---
+++ hello-spring/src/main/java/hello/proxy/trace/logtrace/LogTrace.java
@@ -0,0 +1,11 @@
+package hello.proxy.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+
+public interface LogTrace {
+    TraceStatus begin(String message);
+
+    void end(TraceStatus status);
+
+    void exception(TraceStatus status, Exception e);
+}
