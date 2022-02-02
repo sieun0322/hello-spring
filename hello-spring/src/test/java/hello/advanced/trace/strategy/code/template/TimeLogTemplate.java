f471f7759bebceed010e07737b6ca5a7abf2742a
zion <lce322@naver.com>
2022-02-02 21:32:15 +0900

    로그 추적기 콜백 패턴 예제 [스프링 핵심원리 - 고급편]

---
+++ hello-spring/src/test/java/hello/advanced/trace/strategy/code/template/TimeLogTemplate.java
@@ -0,0 +1,17 @@
+package hello.advanced.trace.strategy.code.template;
+
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeLogTemplate {
+
+    public void execute(Callback callback){
+        long startTime = System.currentTimeMillis();
+        callback.call();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = System.currentTimeMillis();
+        log.info("resultTime={}",resultTime);
+    }
+}
