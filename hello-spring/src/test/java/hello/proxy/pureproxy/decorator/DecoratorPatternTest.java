50a7b2f3d4d3a62923905ad033a8b79c3ccb5cbe
zion <lce322@naver.com>
2022-02-06 00:10:36 +0900

    데코레이터 패턴 -> MessageDecorator 예제 [스프링 핵심원리 - 고급편]

---
+++ hello-spring/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,33 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.*;
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecoratorTest(){
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1(){
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+    @Test
+    void decorator2(){
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+}
