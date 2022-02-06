81a8576da5266d53d04a0c9fd712d53a742a6b29
zion <lce322@naver.com>
2022-02-07 00:13:33 +0900

    인터페이스 있는 구체 클래스에 프록시 적용 [스프링 핵심원리 - 고급편]

--- hello-spring/src/main/java/hello/proxy/ProxyApplication.java
+++ hello-spring/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,21 +1,31 @@
 package hello.proxy;
 
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import hello.core.AppConfig;
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ProxyApplication {
     public static void main(String[] args){
         SpringApplication.run(ProxyApplication.class,args);
+    }
+
+    @Bean
+    public LogTrace logTrace(){
+        return new ThreadLocalLogTrace();
     }
 }
