6c64e6d9a2e8f336f91feb53677e2dd5556bbb43
zion <lce322@naver.com>
2022-02-12 16:18:55 +0900

    프록시 팩토리 - 예제2 [스프링 핵심원리 - 고급편]

--- hello-spring/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ hello-spring/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -42,4 +42,19 @@ public class ProxyFactoryTest {
         assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
         assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
     }
+    @Test
+    @DisplayName("proxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB 사용, 클래스 기반 프록시 사용")
+    void proxyTargetClass(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+        log.info("targetClass={}",target.getClass());
+        log.info("proxyClass={}",proxy.getClass());
+        proxy.save();
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
 }
