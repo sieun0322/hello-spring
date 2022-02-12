355e24a60fe70b5dac298af125aacbe473504e7c
zion <lce322@naver.com>
2022-02-13 00:24:29 +0900

    프록시 팩토리 - 적용1 [스프링 핵심원리 - 고급편]

--- hello-spring/src/main/java/hello/proxy/ProxyApplication.java
+++ hello-spring/src/main/java/hello/proxy/ProxyApplication.java
@@ -5,6 +5,8 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.advice.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.advice.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -20,7 +22,9 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ProxyApplication {
