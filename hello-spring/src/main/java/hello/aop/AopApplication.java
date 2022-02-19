425daebb4e147609ff5bdc654f3f7d97fdb84d80
zion <lce322@naver.com>
2022-02-20 00:33:15 +0900

    스프링 AOP 구현1 - 시작

--- hello-spring/src/main/java/hello/aop/AopApplication.java
+++ hello-spring/src/main/java/hello/aop/AopApplication.java
@@ -1,13 +1,17 @@
 package hello.aop;
 
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@Import(AspectV2.class)
 public class AopApplication {
     public static void main(String[] args){
         SpringApplication.run(AopApplication.class,args);
--- hello-spring/src/main/java/hello/aop/order/OrderService.java
+++ hello-spring/src/main/java/hello/aop/order/OrderService.java
@@ -11,7 +11,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository, LogTrace trace) {
+    public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
     }
     public void orderItem(String itemId){
