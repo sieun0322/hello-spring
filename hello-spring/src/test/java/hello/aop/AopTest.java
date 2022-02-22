5dac0f80448d3433b2f81be89e8ce5be665c3072
zion <lce322@naver.com>
2022-02-23 01:32:57 +0900

    스프링 AOP 구현6 - 어드바이스 종류

--- hello-spring/src/test/java/hello/aop/AopTest.java
+++ hello-spring/src/test/java/hello/aop/AopTest.java
@@ -3,6 +3,7 @@ package hello.aop;
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,8 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest()
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
     @Autowired
     OrderService orderService;
