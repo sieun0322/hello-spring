c2b164a6f133fef7a7562bdbef360c3a5763403e
zion <lce322@naver.com>
2022-02-24 02:29:29 +0900

    스프링 AOP 예제 만들기

---
+++ hello-spring/src/main/java/hello/aop/member/annotation/MemberService.java
@@ -0,0 +1,5 @@
+package hello.aop.member.annotation;
+
+public interface MemberService {
+    String hello(String param );
+}
---
+++ hello-spring/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -0,0 +1,34 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.annotation.MemberServiceImpl;
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV6Advice;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ExecutionTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello",String.class);
+    }
+    @Test
+    void printMethod(){
+        log.info("helloMethod={}",helloMethod);
+    }
+
+}
