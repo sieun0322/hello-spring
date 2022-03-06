648eee4a0360ab31dcb3eda8581a03b405c5237c
zion <lce322@naver.com>
2022-03-07 01:08:23 +0900

    [스프링 핵심 원리 - 고급편] 재시도 AOP

--- hello-spring/src/main/java/hello/aop/exam/ExamRepository.java
+++ hello-spring/src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ public class ExamRepository {
     private static int seq = 0;
 
     @Trace
+    @Retry(value = 4)
     public String save(String itemId){
         seq++;
         if(seq%5 == 0){
--- hello-spring/src/test/java/hello/aop/exam/ExamTest.java
+++ hello-spring/src/test/java/hello/aop/exam/ExamTest.java
@@ -1,6 +1,7 @@
 package hello.aop.exam;
 
 import hello.aop.exam.annotation.Trace;
+import hello.aop.exam.aop.RetryAspect;
 import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Slf4j
-@Import(TraceAspect.class)
+@Import({TraceAspect.class, RetryAspect.class})
 @SpringBootTest
 public class ExamTest {
     @Autowired
