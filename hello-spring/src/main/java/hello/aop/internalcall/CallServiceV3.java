43f605da36044826aff584a44de4fd9dd91a5d0b
zion <lce322@naver.com>
2022-03-10 21:48:53 +0900

    [스프링 핵심 원리 - 고급편] 프록시와 내부 호출 - 대안3 구조변경

---
+++ hello-spring/src/main/java/hello/aop/internalcall/CallServiceV3.java
@@ -0,0 +1,21 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV3 {
+    /**
+     * 대안3 구조변경(분리)
+     */
+    private final InternalService internalService;
+
+    public void external(){
+        log.info("call external");
+        internalService.internal();//외부 메서드 호출
+    }
+}
