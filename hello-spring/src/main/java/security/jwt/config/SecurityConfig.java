9b09ed0a0824b6ae5150e80678c0656764c900d1
zion <lce322@naver.com>
2022-04-08 11:26:08 +0900

    [스프링부트 시큐리티 & JWT 강의] JWT Bearer 인증 방식

--- hello-spring/src/main/java/security/jwt/config/SecurityConfig.java
+++ hello-spring/src/main/java/security/jwt/config/SecurityConfig.java
@@ -17,11 +17,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션 사용X
                 .and()
                 .addFilter(corsFilter)//@CrossOrigin(인증x),시큐리티 필터에 등록 인증o
                 .formLogin().disable()
-                .httpBasic().disable()
+                .httpBasic().disable()//http-authorization : ID,PW 암호화되지 않는다.(Basic 방식)=>https 사용. 토큰: 위험부담 낮춤(Bearer 방식, 유효시간 존재)
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
