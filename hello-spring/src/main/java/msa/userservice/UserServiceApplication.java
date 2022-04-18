9e816a78c5fa6d0a4f1ef7886222ebbef7b494b1
zion <lce322@naver.com>
2022-04-18 21:08:01 +0900

    [Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)] Eureka Service User - client 프로젝트 생성

---
+++ hello-spring/src/main/java/msa/userservice/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package msa.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(scanBasePackages = "msa")
+@EnableEurekaClient
+public class UserServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(UserServiceApplication.class,args);
+    }
+
+}
