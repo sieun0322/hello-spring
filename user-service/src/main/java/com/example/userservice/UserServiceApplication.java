1e4eeff8e4faba4cbbd41e5d703bc12ec700555c
zion <lce322@naver.com>
2022-04-20 21:07:51 +0900

    [Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)] User Service 4포트로 실행

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -4,6 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 여러 어플리케이션 실행
+ * 1. 인텔리제 configuration
+ * 	-Dserver.port=9002
+ * 2. 인텔리제 Terminal
+ *    mvn spring-boot:run -Dspring-boot.run.jvmArguments='-Dserver.port=9003'
+ * 3. iterm
+ * 	mvn clean
+ * 	mvn compile package
+ * 	java -jar -Dserver.port=9004 ./target/user-service-0.0.1-SNAPSHOT.jar
+ *
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class UserServiceApplication {
