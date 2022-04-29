1d0c18d4402acfa01e6188e9f2a63c95fa5ae084
zion <lce322@naver.com>
2022-04-29 23:20:08 +0900

    [Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)] Spring Cloud Gateway - Load Balancer2

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,24 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+
+    Environment env;
+    public FirstServiceController(Environment env){
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the First Service";
@@ -20,7 +29,8 @@ public class FirstServiceController {
         return "Hello word in First Service.";
     }
     @GetMapping("/check")
-    public String check(){
-        return "Hi word in First Service.";
+    public String check(HttpServletRequest request){
+        log.info("server port=",request.getServerPort());
+        return String.format("Hi word in First Service.%s",env.getProperty("local.server.port"));
     }
 }
