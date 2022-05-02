2734da9f8b0f6baf55671c535dce18fcb9e6478b
zion <lce322@naver.com>
2022-05-03 01:21:51 +0900

    [Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)] User Microservice - JPA2

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +19,13 @@ public class UserServiceImpl implements UserService {
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
         ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = mapper.map(userDto,UserEntity.class);
         userEntity.setEncryptedPwd("encrypted_password");
 
         userRepository.save(userEntity);
-        return null;
+
+        UserDto returnUserDto = mapper.map(userEntity,UserDto.class);
+        return returnUserDto;
     }
 }
