91408961cab6e5a9ca50b6ed0ef2bf6a3ed90a1f
zion <lce322@naver.com>
2022-05-02 21:17:35 +0900

    [Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)] User Microservice - JPA1

---
+++ user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.userservice.jpa;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public interface UserRepository extends CrudRepository<UserEntity,Long> {
+
+}
---
+++ user-service/src/main/java/com/example/userservice/vo/RequestUser.java
@@ -0,0 +1,24 @@
+package com.example.userservice.vo;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+//import javax.validation.constraints.Email;
+//import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.Size;
+
+@Data
+public class RequestUser {
+
+    //@NotNull(message = "Email can not be null")
+    //@Size(min = 2, message = "Email not be less than two characters")
+    //@Email
+    private String email;
+
+    //@NotNull(message = "Name can not be null")
+    //@Size(min = 2, message = "Name not be less than two characters")
+    private String name;
+
+    //@NotNull(message = "Password can not be null")
+    //@Size(min = 8, message = "Password must be less than 8 characters")
+    private String pwd;
+}
