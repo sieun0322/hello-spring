28c847001b4a47a0dca27cdbb2e5538710764b9d
zion <lce322@naver.com>
2022-05-12 01:47:41 +0900

    [Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)] Users Microservice - loadUserByUsername() 구현

--- user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
@@ -11,4 +11,6 @@ import java.util.UUID;
 public interface UserRepository extends CrudRepository<UserEntity,Long> {
 
     UserEntity findByUserId(String userId);
+    UserEntity findByEmail(String username);
+
 }
