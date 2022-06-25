57651e79fdb65490ed50bf3f196af542c172a3aa
zion <lce322@naver.com>
2022-06-25 23:49:41 +0900

    [Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)] Users Microservice - Fegin Client ①

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,9 +2,12 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 여러 어플리케이션 실행
@@ -22,6 +25,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
@@ -31,4 +35,11 @@ public class UserServiceApplication {
 	public BCryptPasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
 }
